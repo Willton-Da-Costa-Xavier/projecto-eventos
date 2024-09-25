efc8a5befa26aa7266c3b62f6dcebfd30869e1fa
Willton <willtondacosta@gmail.com>
2024-09-25 10:10:45 +0200

    finalizei o estilo do index

--- WDXeventos/resources/views/layouts/main.blade.php
+++ WDXeventos/resources/views/layouts/main.blade.php
@@ -5,6 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/style.css">
+
+    {{-- favicon --}}
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    {{-- fim --}}
+
+    {{-- bootstarp icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    {{-- fim --}}
+
+    {{-- google icons --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    {{-- fim --}}
+
     <title>@yield('title')</title>
 </head>
 <body>
--- WDXeventos/resources/views/welcome.blade.php
+++ WDXeventos/resources/views/welcome.blade.php
@@ -1,11 +1,123 @@
-@extends('layout.main')
+@extends('layouts.main')
 @section('title', 'WDXeventos')
     
 @section('content')
     <div class="search-container">
-        <h2>Busque um Evento</h2>
+        <h1 class="titulo">Busque um Evento</h1>
         <form action="/" method="GET">
-            <input type="text" name="search" id="search" class="form-search">
+            <input type="text" name="search" id="search" class="form-search" placeholder="Procure aqui os seus eventos">
         </form>
-    </div>
+    </div>{{-- search-container --}}
+
+    <div class="conteudo-principal">
+
+        <div class="conteudo-topo">
+
+            <h2 class="titulo"><i class="bi bi-calendar-event-fill"></i> Seus Eventos</h2>
+
+            <p class="msg">Nao ha eventos registrados</p>
+
+        </div>{{-- conteudo-topo --}}
+
+        <div class="eventos-container">
+
+            <h3 class="titulo"><span class="material-symbols-outlined">
+                celebration
+                </span> Festas
+            </h3>
+
+            <div class="card-container">
+
+                <div class="card">
+                    <img src="/img/banner.jpg" alt="">
+                    <div class="card-body">
+                        <p class="card-date">10/12/2024</p>
+                        <h5 class="card-title">Evento de PHP</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="btn">Saber Mais</a>
+
+                    </div>{{-- card-body --}}
+
+                </div>{{-- card --}}
+
+            </div>{{-- card-container --}}
+
+        </div>{{-- eventos-container --}}
+
+        <div class="eventos-container">
+
+            <h3 class="titulo"><span class="material-symbols-outlined">
+                groups
+                </span> Eventos Sociais
+            </h3>
+
+            <div class="card-container">
+
+                <div class="card">
+                    <img src="/img/banner.jpg" alt="">
+                    <div class="card-body">
+                        <p class="card-date">10/12/2024</p>
+                        <h5 class="card-title">Evento de PHP</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="btn">Saber Mais</a>
+
+                    </div>{{-- card-body --}}
+
+                </div>{{-- card --}}
+
+            </div>{{-- card-container --}}
+
+        </div>{{-- eventos-container --}}
+
+        <div class="eventos-container">
+
+            <h3 class="titulo"><span class="material-symbols-outlined">
+                festival
+                </span> Festivais
+            </h3>
+
+            <div class="card-container">
+
+                <div class="card">
+                    <img src="/img/banner.jpg" alt="">
+                    <div class="card-body">
+                        <p class="card-date">10/12/2024</p>
+                        <h5 class="card-title">Evento de PHP</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="btn">Saber Mais</a>
+
+                    </div>{{-- card-body --}}
+
+                </div>{{-- card --}}
+
+            </div>{{-- card-container --}}
+
+        </div>{{-- eventos-container --}}
+
+        <div class="eventos-container">
+
+            <h3 class="titulo"><span class="material-symbols-outlined">
+                music_note
+                </span> Concertos
+            </h3>
+
+            <div class="card-container">
+
+                <div class="card">
+                    <img src="/img/banner.jpg" alt="">
+                    <div class="card-body">
+                        <p class="card-date">10/12/2024</p>
+                        <h5 class="card-title">Evento de PHP</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="btn">Saber Mais</a>
+
+                    </div>{{-- card-body --}}
+
+                </div>{{-- card --}}
+
+            </div>{{-- card-container --}}
+
+        </div>{{-- eventos-container --}}
+
+    </div>{{-- conteudo-principal --}}
 @endsection
